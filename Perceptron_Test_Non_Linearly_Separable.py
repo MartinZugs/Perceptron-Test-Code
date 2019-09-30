dee6db4342126672e7f3a4d6f9df478f318928af
Martin Zugschwert <martin.e.zugschwert@gmail.com>
2019-09-30 18:02:08 -0500

    Here is all of my code, full disclosure a lot of it came from other sources, but I strung it together with my central logic

---
+++ Perceptron_Test_Non_Linearly_Separable.py
@@ -0,0 +1,37 @@
+import numpy as np
+from Perceptron import Perceptron
+import matplotlib.pyplot as plt
+
+training_inputs = []
+
+training_inputs.append(np.array([0,5]))
+training_inputs.append(np.array([0,6]))
+training_inputs.append(np.array([6,0]))
+training_inputs.append(np.array([5,0]))
+training_inputs.append(np.array([0,7]))
+training_inputs.append(np.array([0,8]))
+training_inputs.append(np.array([7,0]))
+training_inputs.append(np.array([8,0]))
+training_inputs.append(np.array([0,5]))
+training_inputs.append(np.array([5,0]))
+
+
+labels = np.array([1, 0, 0, 1, 1, 0, 0, 1, 1, 0])
+
+perc = Perceptron(2)
+
+perc.train(training_inputs, labels)
+
+plt.plot(range(1, len(perc.errors_a) + 1), perc.errors_a, marker='o')
+plt.xlabel =('Number of updates')
+plt.ylabel=('Number of epochs')
+plt.show()
+
+inputs = []
+
+
+# inputs.append(np.array([0,9]))
+
+inputs.append(np.array([9,0]))
+
+print(perc.predict(inputs))
